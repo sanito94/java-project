b89ff8a94c72b5dd1cccdb0e5556c8aee513b113
sanito94 <122975967+sanito94@users.noreply.github.com>
2024-09-19 16:21:55 +0300

    Changed finding all users by Last Name ascending and then Date of Birth ascending

--- src/main/java/com/example/demo/repo/UserRepository.java
+++ src/main/java/com/example/demo/repo/UserRepository.java
@@ -1,12 +1,17 @@
 package com.example.demo.repo;
 
 import com.example.demo.entity.Employee;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<Employee, Integer> {
-    @Query("SELECT employee from  Employee employee order by employee.lastName asc")
-    List<Employee> findAllByLastName();
+//    @Query("SELECT employee from  Employee employee order by employee.lastName asc")
+//    List<Employee> findAllByLastName();
+
+    List<Employee> findAllByOrderByLastNameAscDateOfBirthAsc();
 }
