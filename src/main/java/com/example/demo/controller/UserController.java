19303a679301a2848a1b95951cfd7bb6688f61d3
sanito94 <122975967+sanito94@users.noreply.github.com>
2024-09-19 16:20:17 +0300

    User controller mapping changed to be as default mapping

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -5,36 +5,38 @@ import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/users")
+@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @PostMapping("/addUser")
+    @PostMapping
     public Employee addUser(@RequestBody Employee user) {
         return userService.createUser(user);
     }
 
-    @GetMapping("/user/{id}")
+    @GetMapping("/{id}")
     public Employee getUser(@PathVariable int id) {
         return userService.getUserById(id);
     }
 
-    @GetMapping("/allUsers")
+    @GetMapping
     public List<Employee> getAllUsers() {
         return userService.getUsers();
     }
 
-    @PutMapping("/updateUser")
-    public Employee updateUser(@RequestBody Employee user) {
-        return userService.updateUser(user);
+    @PutMapping("/{id}")
+    public Employee updateUser(@PathVariable int id, @RequestBody Employee user) {
+        return userService.updateUser(id, user);
     }
 
-    @DeleteMapping("/user/{id}")
+    @DeleteMapping("/{id}")
     public String deleteUser(@PathVariable int id) {
         return userService.deleteUserById(id);
     }
